76ed7f65bf6a0c78b69e21ece92d0bd3bc4655b7
Kanami <kanami.h217@gmail.com>
2021-04-14 11:24:11 -0400

    modified class to className

--- src/components/Chat/ConversationListItem/index.js
+++ src/components/Chat/ConversationListItem/index.js
@@ -1,107 +1,102 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled/macro';
-import moment from 'moment-timezone';
-import 'moment/locale/ja'
-import AvatarOne from '../../../images/avatar1.png';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled/macro";
+import moment from "moment-timezone";
+import "moment/locale/ja";
+import AvatarOne from "../../../images/avatar1.png";
 
-const SEVEN_DAYS_AGO = moment().subtract(7, 'day').format('x');
-const START_OF_YEAR = moment().startOf('year').format('x');
+const SEVEN_DAYS_AGO = moment().subtract(7, "day").format("x");
+const START_OF_YEAR = moment().startOf("year").format("x");
 
-const ListItemWrapper = styled.li({
-  "display": "flex",
-  "flexDirection": "row",
-  "overflowX": "hidden",
-  "padding": "10px",
-  "borderBottom": "1px solid #ddd",
-  "minHeight": "75px"
-}, ({ active }) => {
-  const styles = [];
-  if (active) {
-    styles.push({
-      backgroundColor: '#F4F4F4'
-    })
-  }
-  return styles;
-})
+const ListItemWrapper = styled.li(
+  {
+    display: "flex",
+    flexDirection: "row",
+    overflowX: "hidden",
+    padding: "10px",
+    borderBottom: "1px solid #ddd",
+    minHeight: "75px",
+  },
+  ({ active }) => {
+    const styles = [];
+    if (active) {
+      styles.push({
+        backgroundColor: "#F4F4F4",
+      });
+    }
+    return styles;
+  },
+);
 
 const AvatarWrapper = styled.div({
   marginRight: 15,
-})
+});
 
 const AvatarImage = styled.img({
-  "borderRadius": "50%",
-  "height": "50px",
-  "width": "50px",
-  "backgroundColor": "#F4F4F4"
-})
+  borderRadius: "50%",
+  height: "50px",
+  width: "50px",
+  backgroundColor: "#F4F4F4",
+});
 
 const ChatUserWrapper = styled.div({
-  "display": "flex",
-  "flex": "1 1 auto",
-  "justifyContent": "center",
-  "minWidth": "0",
-  "padding": "0 12px 0 0",
-  "flexDirection": "column"
-})
+  display: "flex",
+  flex: "1 1 auto",
+  justifyContent: "center",
+  minWidth: "0",
+  padding: "0 12px 0 0",
+  flexDirection: "column",
+});
 
 const ChatUserUpper = styled.div({
-  "display": "flex",
-  "flexDirection": "row",
-  "justifyContent": "space-between"
-})
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+});
 
 const LastChatTime = styled.div({
-  "color": "#9E9F9F",
-  "fontSize": "0.9em",
-})
+  color: "#9E9F9F",
+  fontSize: "0.9em",
+});
 
 const LastMessage = styled.div({
-  "fontSize": "0.9em",
-  "color": "#9E9F9F",
-  "overflow": "hidden",
-  "textOverflow": "ellipsis",
-  "whiteSpace": "nowrap"
-})
+  fontSize: "0.9em",
+  color: "#9E9F9F",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+});
 
 const getFormattedDate = (unixTime) => {
-  moment.locale('ja'); // ロケールを日本語にセットします。
-} 
+  moment.locale("ja"); // ロケールを日本語にセットします。
+};
 
-const ConversationListItem = ({ 
+const ConversationListItem = ({
   handleChooseConversation,
-  conversation, 
-  isChosen
+  conversation,
+  isChosen,
 }) => {
   return (
     <ListItemWrapper active={isChosen}>
       <AvatarWrapper>
-        <AvatarImage
-          alt='アバター'
-          src={conversation.user.avatarUrl} />
+        <AvatarImage alt="アバター" src={conversation.user.avatarUrl} />
       </AvatarWrapper>
       <ChatUserWrapper>
         <ChatUserUpper>
-          <div class="chat-user-name">
-            {conversation.user.name}
-          </div>
-          <LastChatTime>
-            時間を表示
-          </LastChatTime>
+          <div className="chat-user-name">{conversation.user.name}</div>
+          <LastChatTime>時間を表示</LastChatTime>
         </ChatUserUpper>
-        <LastMessage>
-          {conversation.lastMessage.body}
-        </LastMessage>
+        <LastMessage>{conversation.lastMessage.body}</LastMessage>
       </ChatUserWrapper>
     </ListItemWrapper>
   );
-}
+};
 
 ConversationListItem.propTypes = {
   conversation: PropTypes.object.isRequired,
   isChosen: PropTypes.bool,
-  handleChooseConversation: PropTypes.func
-}
+  handleChooseConversation: PropTypes.func,
+};
 
 ConversationListItem.defaultProps = {
   isChosen: false,
@@ -110,10 +105,9 @@ ConversationListItem.defaultProps = {
     name: "会話相手",
     lastMessage: {
       sentAt: 1557905239000,
-      body: "こんにちは!"
-    }
-  }
-}
+      body: "こんにちは!",
+    },
+  },
+};
 
 export default ConversationListItem;
-
